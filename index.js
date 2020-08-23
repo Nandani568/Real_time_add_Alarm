f834a7fed7bfe09bd7e07169f4439124e3dd4e3b
Nandani568 <nandanisah35@gmail.com>
2020-08-24 01:58:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+let a;
+let date;
+let time;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+setInterval(() => {
+    a = new Date();
+    date = a.toLocaleDateString(undefined, options);
+    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    document.getElementById('time').innerHTML = time + "<br>on " + date;
+}, 1000);
+
+
+
+
+
+// alarm clock
+const alarmSubmit = document.getElementById('alarmSubmit');
+
+// Add an event listener to the submit button
+alarmSubmit.addEventListener('click', setAlarm);
+
+var audio = new Audio('love.mp3');
+
+// function to play the alarm ring tone
+function ringBell() {
+    audio.play();
+}
+
+// This function will run whenever alarm is set from the UI
+function setAlarm(e) {
+    e.preventDefault();
+    const alarm = document.getElementById('alarm');
+    alarmDate = new Date(alarm.value);
+    console.log(`Setting Alarm for ${alarmDate}...`);
+    now = new Date();
+
+    let timeToAlarm = alarmDate - now;
+    console.log(timeToAlarm);
+    if(timeToAlarm>=0){
+        setTimeout(() => {
+            console.log("Ringing now")
+            ringBell();
+        }, timeToAlarm);
+    }
+}
